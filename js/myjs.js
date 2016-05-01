992c05da360bb995b26b4c1decd683872b2e3f15
Richard Yee <richtomyee@gmail.com>
2016-05-01 11:37:58 -0400

    test

---
+++ js/myjs.js
@@ -0,0 +1,20 @@
+var submitData= function(){
+  var urlBase = "http://gateway-a.watsonplatform.net/calls/url/URLGetRankedNamedEntities?";
+  var apiKey= "12185cdb4698b0f203b59d58f0b11d0ff2b6e546";
+  urlBase = urlBase + "apikey="+apiKey;
+  var inputText= $("#exemplar").val();
+  urlBase = urlBase+"&url=" + encodeURIComponent(inputText);
+  urlBase = urlBase + "&outputMode=json";
+  urlBase = urlBase + "&showSourceText=1"
+  console.log(urlBase);
+
+
+  $.ajax({
+    url: urlBase,
+    type: 'POST',
+    success: function(data) {
+      console.log(data);
+    }
+
+  });
+}
